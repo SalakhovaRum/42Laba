4651c0a2e77cf22438b12160db739e050477d5bb
Rumiya <littleangel03@mail.ru>
2023-07-31 14:43:19 +0300

    first commit

--- app/databases/connect.php
+++ app/databases/connect.php
@@ -1,1 +1,19 @@
 <?php
+
+$driver = 'mysql';
+$host = 'localhost';
+$db_name = 'laba';
+$db_user = 'root';
+$db_pass = 'mysql';
+$charset = 'utf8';
+$options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
+
+try {
+    //создаем экземпляр класса
+    $pdo = new PDO(
+        "$driver:host=$host;dbname=$db_name;charset=$charset",
+        $db_user, $db_pass, $options
+    );
+}catch (PDOException $i){
+    die("Ошибка подключения к базе данных");
+}
