f4b88b2aa6107b0a61f56aa579e0aaf6b312cb00
Rumiya <littleangel03@mail.ru>
2023-08-01 10:56:33 +0300

    Доработка функции запросов к базе данных

--- app/databases/db.php
+++ app/databases/db.php
@@ -5,28 +5,88 @@ require('connect.php');
 function tt($value){
     echo '<pre>';
     print_r($value);
-    echo '</pre';
+    echo '</pre>';
 }
 
-function selectAll($table){
-    global $pdo;
-    // SQL - запрос
-    $sql = "SELECT * from $table";
-    // Подготовка запроса
-    $query = $pdo->prepare($sql);
-    // Отправляем сам запрос
-    $query->execute();
-    // Проверка на ошибки
+// Проверка на ошибки
+function dbCheckError($query){
     $errInfo = $query->errorInfo();
 
     if ($errInfo[0] !== PDO::ERR_NONE){
         echo $errInfo[2];
         exit();
     }
+    return true;
+}
 
+// Запрос на получение данных c одной таблицы
+function selectAll($table, $params = []){
+    global $pdo;
+    // SQL - запрос
+    $sql = "SELECT * from $table";
+
+    if (!empty($params)){
+        $i = 0;
+        foreach ($params as $key => $value){
+            if (!is_numeric($value)){
+                $value = "'".$value."'";
+            }
+            if ($i === 0){
+                $sql = $sql . " WHERE $key = $value";
+            }else{
+                $sql = $sql . " AND $key = $value";
+            }
+            $i++;
+        }
+
+    }
+
+    // Подготовка запроса
+    $query = $pdo->prepare($sql);
+    // Отправляем сам запрос
+    $query->execute();
+    dbCheckError($query);
 // Получение данных
     return $query->fetchAll();
 }
 
-tt(selectAll('users'));
 
+// Запрос на получение данных c одной строки с выбранной таблицы
+function selectOne($table, $params = []){
+    global $pdo;
+    // SQL - запрос
+    $sql = "SELECT * from $table";
+
+    if (!empty($params)){
+        $i = 0;
+        foreach ($params as $key => $value){
+            if (!is_numeric($value)){
+                $value = "'".$value."'";
+            }
+            if ($i === 0){
+                $sql = $sql . " WHERE $key = $value";
+            }else{
+                $sql = $sql . " AND $key = $value";
+            }
+            $i++;
+        }
+
+    }
+//    $sql = $sql . " LIMIT 1 ";
+    // Подготовка запроса
+    $query = $pdo->prepare($sql);
+    // Отправляем сам запрос
+    $query->execute();
+    dbCheckError($query);
+// Получение данных
+    return $query->fetch();
+}
+
+$params = [
+    'admin' => 1,
+    'email' => 'littleangel03@mail.ru'
+];
+
+//tt(selectAll('users', $params));
+
+tt(selectOne('users'));
